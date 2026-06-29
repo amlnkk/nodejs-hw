c688dda62b4cbce81f521315f9df087d4777deed
alinamelnik041@gmail.com <alinamelnik041@gmail.com>
2026-06-30 00:52:04 +0300

    fix: use instanceof HttpError in errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,7 @@
-import { isHttpError } from 'http-errors';
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, _next) => {
-  if (isHttpError(err)) {
+  if (err instanceof HttpError) {
     return res.status(err.status).json({ message: err.message });
   }
   res.status(500).json({ message: err.message });
